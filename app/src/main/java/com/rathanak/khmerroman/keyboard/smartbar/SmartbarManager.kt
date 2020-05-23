50ba53be05aa72c88a342a5bd6906ae04f2a4a92
Sreang Rathanak <sreangrathanak@yahoo.com>
2020-05-23 09:39:05 +0700

    Uodate change

--- app/src/main/java/com/rathanak/khmerroman/keyboard/smartbar/SmartbarManager.kt
+++ app/src/main/java/com/rathanak/khmerroman/keyboard/smartbar/SmartbarManager.kt
@@ -4,9 +4,12 @@ import android.view.View
 import android.view.textservice.SentenceSuggestionsInfo
 import android.view.textservice.SpellCheckerSession
 import android.view.textservice.SuggestionsInfo
+import android.widget.CompoundButton
 import android.widget.LinearLayout
+import android.widget.ToggleButton
 import com.rathanak.khmerroman.R
 import com.rathanak.khmerroman.keyboard.R2KhmerService
+import kotlinx.android.synthetic.main.smartbar.view.*
 
 class SmartbarManager(
     private val r_2_khmer: R2KhmerService
@@ -22,6 +25,13 @@ class SmartbarManager(
     fun createSmartbarView(): LinearLayout {
         val smartbarView = View.inflate(r_2_khmer.context, R.layout.smartbar, null) as LinearLayout
         this.smartbarView = smartbarView
+        this.smartbarView!!.toggleOption!!.setOnCheckedChangeListener { buttonView, isChecked ->
+            if (isChecked) {
+                buttonView.setBackgroundResource(R.drawable.ic_mini_logo)
+            } else {
+                buttonView.setBackgroundResource(R.drawable.ic_chevron_right)
+            }
+        }
 
         return smartbarView
     }
