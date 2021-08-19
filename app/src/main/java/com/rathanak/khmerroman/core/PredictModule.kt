7f0e5f3cb5f6288894b8ecb851f66b3e0c16c0d3
Sreang Rathanak <sreangrathanak@yahoo.com>
2021-08-20 00:05:27 +0700

    Remove eb config file

--- app/src/main/java/com/rathanak/khmerroman/core/PredictModule.kt
+++ app/src/main/java/com/rathanak/khmerroman/core/PredictModule.kt
@@ -1,16 +1,6 @@
 package com.rathanak.khmerroman.core
 
-import android.util.Log
-import org.koin.android.ext.koin.androidApplication
 import org.koin.dsl.module.module
-import java.io.ObjectInputStream
-import kotlinx.coroutines.async
-// my libs
-import com.rathanak.nlp.NGrams
-import com.rathanak.nlp.LanguageModel
-import com.rathanak.nlp.RankingModel
-import com.rathanak.nlp.StupidBackoffRanking
-import kotlinx.coroutines.Dispatchers
 
 val predictModule = module {
 //    single {
