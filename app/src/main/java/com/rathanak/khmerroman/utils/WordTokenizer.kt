9ca7b2665240ff6d00c4d31bd802df4e01c8d58b
Sreang Rathanak <sreangrathanak@yahoo.com>
2021-08-20 21:57:15 +0700

    Hello

--- app/src/main/java/com/rathanak/khmerroman/utils/WordTokenizer.kt
+++ app/src/main/java/com/rathanak/khmerroman/utils/WordTokenizer.kt
@@ -1,29 +1,62 @@
 package com.rathanak.khmerroman.utils
 
 class WordTokenizer {
-    fun tokenize(inputText: String): Array<String> {
+    fun coreSegment(inputText: String): MutableList<String> {
         val words: MutableList<String> = arrayListOf()
         inputText.split("[\n\t\b​ ]".toRegex()).forEach {
-            var tmp = it
-            if (tmp.length <= 1) {
-                words.add(tmp)
+            var word = it
+            if (word.length <= 1) {
+                words.add(word)
+                return@forEach
+            }
+
+            // if Khmer word
+            //    if Khmer word and correct
+            //       No segment
+            //    else
+            //       if long then max length
+            //          break line and seg
+            //       else
+            //          break whole text
+            if (word[0] in 'ក'..'ឳ') {
+//                if (true) {
+//
+//                } else {
+//
+//                }
+                //    remove later
+                words.add(word)
             } else {
-                if (CHAR_SYMBOL.contains(tmp[0])) {
-                    words.add(tmp[0].toString())
-                    tmp = tmp.substring(1, tmp.length)
-                }
-                if (CHAR_SYMBOL.contains(tmp[tmp.length - 1])) {
-                    words.add(tmp.substring(0, tmp.length - 1))
-                    words.add(tmp[tmp.length - 1].toString())
-                } else {
-                    words.add(tmp)
-                }
+                words.add(word)
+            }
+        }
+        return words
+    }
+
+    fun tokenize(inputText: String): Array<String> {
+        val words: MutableList<String> = arrayListOf()
+        coreSegment(inputText).forEach {
+            var word = it
+            if (word.length <= 1) {
+                words.add(word)
+                return@forEach
+            }
+
+            if (CHAR_SYMBOL.contains(word[0])) {
+                words.add(word[0].toString())
+                word = word.substring(1, word.length)
+            }
+            if (CHAR_SYMBOL.contains(word[word.length - 1])) {
+                words.add(word.substring(0, word.length - 1))
+                words.add(word[word.length - 1].toString())
+            } else {
+                words.add(word)
             }
         }
         return words.toTypedArray()
     }
 
     companion object {
-        var CHAR_SYMBOL = arrayOf('.', '?', '!', '"', ',', '(', ')')
+        var CHAR_SYMBOL = arrayOf('.', '?', '!', '"', ',', '(', ')', '។', '៕', '៖', '៘', '៚')
     }
 }
