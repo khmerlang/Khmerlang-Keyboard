fc8f6b11438944cecb885d9baf71d44ef5f02969
Sreang Rathanak <sreangrathanak@yahoo.com>
2021-09-05 12:19:00 +0700

    Update README

--- app/src/main/java/com/rathanak/khmerroman/view/About.kt
+++ app/src/main/java/com/rathanak/khmerroman/view/About.kt
@@ -1,9 +1,14 @@
 package com.rathanak.khmerroman.view
 
+import android.content.Intent
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import com.rathanak.khmerroman.R
+import kotlinx.android.synthetic.main.activity_about.*
+import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.activity_main.txtKhmerlang
 
 class About : AppCompatActivity() {
 
@@ -11,10 +16,34 @@ class About : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_about)
 
-        val btnBack = findViewById(R.id.btn_back) as Button
         btnBack.setOnClickListener {
             // Go back
             super.onBackPressed()
+        }
+
+        btnEmail.setOnClickListener {
+            val intent = Intent(Intent.ACTION_VIEW, Uri.parse("mailto:sreangrathanak@yahoo.com"))
+            startActivity(intent)
+        }
+
+        btnFb.setOnClickListener {
+            val intent = Intent(Intent.ACTION_VIEW, Uri.parse("https://web.facebook.com/khmerlang.official"))
+            startActivity(intent)
+        }
+
+        btnGithub.setOnClickListener {
+            val intent = Intent(Intent.ACTION_VIEW, Uri.parse("https://github.com/khmerlang"))
+            startActivity(intent)
+        }
+
+        btnWeb.setOnClickListener {
+            val intent = Intent(Intent.ACTION_VIEW, Uri.parse("www.khmerlang.com"))
+            startActivity(intent)
+        }
+
+        txtKhmerlang.setOnClickListener {
+            val intent = Intent(Intent.ACTION_VIEW, Uri.parse("www.khmerlang.com"))
+            startActivity(intent)
         }
     }
 }
--- app/src/main/java/com/rathanak/khmerroman/view/MainActivity.kt
+++ app/src/main/java/com/rathanak/khmerroman/view/MainActivity.kt
@@ -2,6 +2,7 @@ package com.rathanak.khmerroman.view
 
 import android.content.Context
 import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
 import android.os.Handler
 import android.provider.Settings
@@ -57,6 +58,10 @@ class MainActivity : AppCompatActivity() {
         btnClose.setOnClickListener {
             moveTaskToBack(true);
             finish()
+        }
+        txtKhmerlang.setOnClickListener {
+            val intent = Intent(Intent.ACTION_VIEW, Uri.parse("www.khmerlang.com"))
+            startActivity(intent)
         }
     }
 
