8e8aa5a86ea1cb4eda56cb8a36f49d960ef4dacb
Sreang Rathanak <sreangrathanajk@yahoo.com>
2023-12-21 17:05:11 +0700

    update button styler

--- app/src/main/java/com/rathanak/khmerroman/keyboard/smartbar/SpellSuggestionAdapter.kt
+++ app/src/main/java/com/rathanak/khmerroman/keyboard/smartbar/SpellSuggestionAdapter.kt
@@ -1,14 +1,18 @@
 package com.rathanak.khmerroman.keyboard.smartbar
 
 import android.content.Context
+import android.view.ContextThemeWrapper
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.BaseAdapter
 import android.widget.Button
 import android.widget.ImageButton
+import android.widget.LinearLayout
 import android.widget.ListView
 import android.widget.TextView
+import androidx.core.view.marginBottom
+import androidx.core.view.marginEnd
 import androidx.recyclerview.widget.RecyclerView
 import com.google.android.flexbox.FlexboxLayout
 import com.rathanak.khmerroman.R
@@ -39,17 +43,22 @@ class SpellSuggestionAdapter(private val context: Context, private val suggestio
         }
 
         val wordsSuggestionList = convertView.findViewById(R.id.wordsSuggestionList) as FlexboxLayout
+        val params = LinearLayout.LayoutParams(
+            LinearLayout.LayoutParams.WRAP_CONTENT,
+            LinearLayout.LayoutParams.WRAP_CONTENT
+        )
+        params.setMargins(4,4,4,4)
         suggestionsList[position].wordsSuggestion.forEach {
-            val btnWord = Button(context)
+            val btnWord = Button(ContextThemeWrapper(context, R.style.ButtonSpellSuggestion), null, R.style.ButtonSpellSuggestion)
+            btnWord.minHeight = 0
+            btnWord.minWidth = 0
             btnWord.text = it.word
-            //TODO: set style
-
             btnWord.setOnClickListener {
                 suggestionsList.removeAt(position); // remove the item from the data list
                 notifyDataSetChanged();
             }
 
-            wordsSuggestionList.addView(btnWord)
+            wordsSuggestionList.addView(btnWord, params)
         }
 
         return convertView
