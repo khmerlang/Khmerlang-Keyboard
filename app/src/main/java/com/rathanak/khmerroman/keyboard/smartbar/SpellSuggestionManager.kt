5048195474ae48073b461377a7b05804c48cf4b3
Sreang Rathanak <sreangrathanajk@yahoo.com>
2023-12-22 09:54:57 +0700

    Mock word suggestion list

--- app/src/main/java/com/rathanak/khmerroman/keyboard/smartbar/SpellSuggestionManager.kt
+++ app/src/main/java/com/rathanak/khmerroman/keyboard/smartbar/SpellSuggestionManager.kt
@@ -1,23 +1,22 @@
 package com.rathanak.khmerroman.keyboard.smartbar
 
-import android.util.Log
 import android.view.View
-import android.view.inputmethod.ExtractedTextRequest
-import android.widget.Button
 import android.widget.LinearLayout
 import androidx.core.graphics.drawable.DrawableCompat
-import androidx.core.view.children
 import com.rathanak.khmerroman.R
 import com.rathanak.khmerroman.keyboard.R2KhmerService
 import com.rathanak.khmerroman.keyboard.common.Styles
 import kotlinx.android.synthetic.main.smartbar.view.btnOpenApp
-import kotlinx.android.synthetic.main.smartbar.view.numbersList
 import kotlinx.android.synthetic.main.smartbar.view.smartbar
 import kotlinx.android.synthetic.main.spell_suggestion.view.spellSuggestionList
+import kotlinx.android.synthetic.main.spell_suggestion.view.noDataText
+import kotlinx.android.synthetic.main.spell_suggestion.view.noInternetConnection
 
 class SpellSuggestionManager(private val r_2_khmer: R2KhmerService) {
     var spellSuggestionView: LinearLayout? = null
     private var isDarkMood: Boolean = false
+    var spellSuggestionAdapter: SpellSuggestionAdapter? = null
+    var spellSuggestionItems: ArrayList<SpellSuggestionItem> = ArrayList()
     fun createSpellSuggestionView(): LinearLayout {
         var spellSuggestionView = View.inflate(r_2_khmer.context, R.layout.spell_suggestion, null) as LinearLayout
         this.spellSuggestionView = spellSuggestionView
@@ -32,16 +31,22 @@ class SpellSuggestionManager(private val r_2_khmer: R2KhmerService) {
         wordsList.add(WordSuggestion("ការពិនិត្យ", 14, 22))
         wordsList.add(WordSuggestion("ការពិនិត្យ", 14, 22))
         wordsList.add(WordSuggestion("ការពិនិត្យ", 14, 22))
+        spellSuggestionItems.add(SpellSuggestionItem("កាពិនិត្យ", wordsList))
+        spellSuggestionItems.add(SpellSuggestionItem("កាពិនិត្យ", wordsList))
+        spellSuggestionItems.add(SpellSuggestionItem( "កាពិនិត្យ", wordsList))
+        spellSuggestionItems.add(SpellSuggestionItem( "កាពិនិត្យ", wordsList))
+        spellSuggestionItems.add(SpellSuggestionItem( "កាពិនិត្យ", wordsList))
+        spellSuggestionItems.add(SpellSuggestionItem( "កាពិនិត្យ", wordsList))
 
-        var arrayList: ArrayList<SpellSuggestionItem> = ArrayList()
-        arrayList.add(SpellSuggestionItem("កាពិនិត្យ", wordsList))
-        arrayList.add(SpellSuggestionItem("កាពិនិត្យ", wordsList))
-        arrayList.add(SpellSuggestionItem( "កាពិនិត្យ", wordsList))
-        arrayList.add(SpellSuggestionItem( "កាពិនិត្យ", wordsList))
-        arrayList.add(SpellSuggestionItem( "កាពិនិត្យ", wordsList))
-        arrayList.add(SpellSuggestionItem( "កាពិនិត្យ", wordsList))
-        var adapter = SpellSuggestionAdapter(r_2_khmer, r_2_khmer.context, arrayList)
-        listView.adapter = adapter
+        spellSuggestionAdapter = SpellSuggestionAdapter(r_2_khmer, r_2_khmer.context, spellSuggestionItems)
+        listView.adapter = spellSuggestionAdapter
+        val emptyView = spellSuggestionView.noDataText
+        var noInternet = spellSuggestionView.noInternetConnection
+        if(true) {
+            listView.emptyView = emptyView
+        } else {
+            listView.emptyView = noInternet
+        }
 
         return spellSuggestionView
     }
