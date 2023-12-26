791e2ece095d77af42ba53dc1730721698939407
Sreang Rathanak <sreangrathanak@yahoo.com>
2023-12-26 21:54:08 +0700

    Add rest call

--- app/src/main/java/com/rathanak/khmerroman/request/ApiService.kt
+++ app/src/main/java/com/rathanak/khmerroman/request/ApiService.kt
@@ -2,9 +2,7 @@ package com.rathanak.khmerroman.request
 
 import retrofit2.Call
 import retrofit2.http.Body
-import retrofit2.http.GET
 import retrofit2.http.POST
-import retrofit2.http.Path
 
 interface ApiService {
     @POST("/v1/spelling-check")
