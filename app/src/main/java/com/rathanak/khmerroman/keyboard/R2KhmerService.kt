5cd18470aaaa7408b0e957eccc4d48c221643734
Sreang Rathanak <sreangrathanajk@yahoo.com>
2023-12-27 20:04:16 +0700

    Add add post request

--- app/src/main/java/com/rathanak/khmerroman/keyboard/R2KhmerService.kt
+++ app/src/main/java/com/rathanak/khmerroman/keyboard/R2KhmerService.kt
@@ -503,7 +503,7 @@ class R2KhmerService : InputMethodService(), KeyboardActionListener {
         currentInputConnection.setComposingRegion(startPos, endPos);
         currentInputConnection.setComposingText(selectText, startPos)
         currentInputConnection.finishComposingText()
-        currentInputConnection.setSelection(startPos, startPos + selectText.length)
+        currentInputConnection.setSelection(startPos + selectText.length, startPos + selectText.length)
         currentInputConnection.endBatchEdit()
     }
 
